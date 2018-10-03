fba481b6797702f6dfe64a1c82b3ed3e1d8c2ce5
Cait Payne <caitpayne@outlook.com>
2018-10-03 14:06:16 -0600

    Assignment Complete

---
+++ goodbyes.js
@@ -0,0 +1,7 @@
+exports.goodbyeEnglish = () => console.log("Goodbye");
+
+exports.goodbyeSpanish = () => console.log("Adios");
+
+exports.goodbyeIcelandic = () => console.log("Bless");
+
+exports.goodbyeJapanese = () => console.log("Sayonara");
---
+++ main.js
@@ -0,0 +1,19 @@
+const greetings = require("./greeting.js");
+
+const goodbyes = require("./goodbyes.js");
+
+// Hello / Goodbye
+greetings.greetingEnglish();
+goodbyes.goodbyeEnglish();
+
+// Konichiwa / Sayonara
+greetings.greetingJapanese();
+goodbyes.goodbyeJapanese();
+
+// Hola / Adios
+greetings.greetingSpanish();
+goodbyes.goodbyeSpanish();
+
+// Hallo / Bless
+greetings.greetingIcelandic();
+goodbyes.goodbyeIcelandic();
